f46768e3f07a2275fc851c815c2d10383779b5da
inachenyuxuan <59850205+inachenyuxuan@users.noreply.github.com>
2021-08-30 12:44:13 -0400

    Set up basic interface

---
+++ blood_calculator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 30 12:41:14 2021
+
+@author: chenyuxuan
+"""
+
+def interface():
+    print("Blood Calculator")
+    print("Make a choice")
+    print("9 - Quit")
+    choice = input("Make a choice: ")
+    print(choice)
+    return choice
+
+interface()
