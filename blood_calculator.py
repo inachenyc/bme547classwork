b5b399fd87dd6905064e41f6e9f4554d291d752f
inachenyuxuan <59850205+inachenyuxuan@users.noreply.github.com>
2021-08-30 12:58:26 -0400

    Added thec choice validation feature

--- blood_calculator.py
+++ blood_calculator.py
@@ -8,10 +8,17 @@ Created on Mon Aug 30 12:41:14 2021
 
 def interface():
     print("Blood Calculator")
-    print("Make a choice")
-    print("9 - Quit")
-    choice = input("Make a choice: ")
+    keep_running = True
+    while keep_running:
+        print("Make a choice")
+        print("9 - Quit")
+        choice = int(input("Make a choice: ")) # so that can compare against 9 later
+        print(type(choice)) #check: choice was a string
+        if choice == 9: # double equal sign for comparison
+            keep_running = False
+            
     print(choice)
     return choice
+
 
 interface()
